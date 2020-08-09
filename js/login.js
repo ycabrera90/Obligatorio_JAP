17821074fb94d6b955d85f1ce2b3c960ae9d6b88
ycabrera90 <68780363+ycabrera90@users.noreply.github.com>
2020-08-08 21:04:46 -0300

    desarrollo del login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,20 @@
+function showLogin(){
+    window.location="login.html";
+}
+
+function entrar(){
+    window.location="index.html";
+}
+
+
+
+getJSONData("json/login.js").then(function(resultObj){
+    if (resultObj.status === "ok"){
+        console.log(resultObj.data);
+    }
+})
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
