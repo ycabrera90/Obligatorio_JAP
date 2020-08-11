52a7b811b57381a114413fd6a0160d8fea6f5f31
ycabrera90 <68780363+ycabrera90@users.noreply.github.com>
2020-08-10 21:49:33 -0300

    redireccionamiento a product-info.html

--- js/products.js
+++ js/products.js
@@ -20,7 +20,7 @@ function showProductsList(){
         {
 
             htmlContentToAppend += `
-            <a href="category-info.html" class="list-group-item list-group-item-action">
+            <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
                         <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
