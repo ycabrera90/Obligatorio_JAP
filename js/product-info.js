35f576541ca651539a504a0479a6b3cebe69652b
ycabrera90 <68780363+ycabrera90@users.noreply.github.com>
2020-09-05 13:03:36 -0300

    se le añadio una lista de productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -53,17 +53,52 @@ function formatedName(nameRaw){
     return formatedName
 }
 function sendComment(){
-let htmlContentToAppend = '';
-let currentDateTime = new Date().getDate()+'/'+(new Date().getMonth()+1)+'/'+new Date().getFullYear()+' '+new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds()
-//htmlContentToAppend = showStars(comments[i].score)
-htmlContentToAppend +=`
-    <p><span>`+currentDateTime+`</span>: <span>`+ localStorage.getItem('user_loged') +`</span>
-    <br>`+document.getElementById('myComment').value+`</p>`
-document.getElementById('comments-list-container').innerHTML += htmlContentToAppend
-
-
+    let htmlContentToAppend = '';
+    let currentDateTime = new Date().getDate()+'/'+(new Date().getMonth()+1)+'/'+new Date().getFullYear()+' '+new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds()
+    htmlContentToAppend = showStars(document.getElementById('myScore').value)
+    htmlContentToAppend +=`
+        <p><span>`+currentDateTime+`</span>: <span>`+ localStorage.getItem('user_loged') +`</span>
+        <br>`+document.getElementById('myComment').value+`</p>`
+    document.getElementById('comments-list-container').innerHTML += htmlContentToAppend
+    document.getElementById('myComment').value = '';        //limpio el área de texto
 }
+function showRelatedProductos(retaledProductos){
+    console.log(retaledProductos)
+    //let productsList = [];
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        console.log(retaledProductos)
+        if (resultObj.status === "ok"){productsList = resultObj.data;}
+        
+        console.log(productsList)
 
+        for(let i = 0; i < retaledProductos.length; i++)
+        {
+            n = retaledProductos[i]         //ubicacion de los productos relacionados en la lista de productos
+
+            // muetro productos relacionados
+            let htmlContentToAppend = ''
+            htmlContentToAppend += `
+            <a class="col-lg-3 col-md-4 col-6" href="product-info.html" style="text-decoration: none; ">
+                <div class="d-block mb-4 h-100">
+                    <h4>`+productsList[n].name+`</h4>
+                    <img class="img-fluid img-thumbnail" src=`+productsList[n].imgSrc+` alt="">
+                </div>
+            </a>
+            `
+            document.getElementById('relatedProducts').innerHTML += htmlContentToAppend
+        }
+        
+
+
+
+    });
+
+    
+
+
+
+    
+}
 
 
 
@@ -82,19 +117,19 @@ document.addEventListener("DOMContentLoaded", function(e){
             let productCurrencyHTML = document.getElementById("productCurrency");
             let productSoldCountHTML = document.getElementById("productSoldCount");
             let productCategoryHTML = document.getElementById("productCategory");
-
-            
-
+                        
             productNameHTML.innerHTML = products.name;
             productDescriptionHTML.innerHTML = products.description;
             productCosttHTML.innerHTML = products.cost;
             productCurrencyHTML.innerHTML = products.currency;
             productSoldCountHTML.innerHTML = products.soldCount;
             productCategoryHTML.innerHTML = products.category;
-                       
-
-            //Muestro las imagenes en forma de galería
+            
+            // Muestro las imagenes en forma de galería
             showImagesGallery(products.images);
+
+            // muestro productos relacionados
+            showRelatedProductos(products.relatedProducts)
         }
     });
 
@@ -103,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         {
             comments = resultObj.data;
             console.log(comments[0].score)
-
+            // muestro los comentarios
             for(let i = 0; i < comments.length; i++){
                 let htmlContentToAppend = ''
                 htmlContentToAppend = showStars(comments[i].score)
@@ -112,27 +147,12 @@ document.addEventListener("DOMContentLoaded", function(e){
                 <br>`+comments[i].description+`</p>`
                 document.getElementById('comments-list-container').innerHTML += htmlContentToAppend
             }
-
-            
-            
-            
-
-
-            
-            
-            
-            
-            
-
-            
-            
-            
-
-            
-                       
-
-            
         }
     });
+
+
+
+
+    
 
 });
